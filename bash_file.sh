39eaf0a526dc726a31ac316ef6c8852392c19e7c
bilal <bilal45.grt@gmail.com>
2024-06-26 18:43:51 +0500

    Add bash files

---
+++ bash_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Display system name
+echo "System Name: $(hostname)"
+
+# Number of running CPUs
+echo "Number of CPUs: $(nproc --all)"
+
+# RAM information
+echo "RAM (Memory) Information:"
+free -h
+
+# HDD information
+echo "HDD (Disk) Usage:"
+df -h
